fbcd1bd69b75e94384af7899421cd79e9c57325d
Youngpuffy <levimuturi2@gmail.com>
2025-05-15 13:47:58 +0300

    object-inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!" 
+    
+    def fill_up_tank(self):
+        return "filling up!"
